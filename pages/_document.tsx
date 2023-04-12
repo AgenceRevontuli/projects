70b0337e0d70c19357310b28331f6d2fb6d177bb
Tristan Tornatore <tristan@revontuli.fr>
2023-04-12 15:47:28 +0200

    Add Footer component / Improve Project page design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Html, Head, Main, NextScript } from 'next/document'
 
@@ -9,6 +10,7 @@ export default function Document() {
         <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   )
--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,10 +1,65 @@
 import { NextPage } from "next"
+import Image from "next/image";
 
 const ProjectItem: NextPage = () => {
+
+    const style = { "--value": 30 } as React.CSSProperties
+
     return(
-        <>
-            Ceci est un projet
-        </>
+        <div>
+            <div className="hero-content flex-col lg:flex-row bg-base-200">
+                <Image 
+                    src="https://picsum.photos/id/237/200/300"
+                    alt="Photo à changer"
+                    width={200}
+                    height={100}
+                    className="max-w-sm rounded-lg shadow-2xl"
+                />
+                <div>
+                    <h1 className="text-5xl font-bold text-center m-4">Réseau multisites Wordpress</h1>
+                    <div className="my-6 flex justify-between items-center px-2">
+                        <div>
+                            <p>Client : <span className="badge badge-secondary font-bold">FNPSL</span></p>
+                        </div>
+                        <div>
+                            <p className="border-solid border-2 border-white py-2 px-4 rounded-lg font-bold">Prix : 23 000 € HT</p>
+                        </div>
+                        <div>
+                            <p>Progression du projet : <span className="radial-progress" style={style}>30%</span></p>
+                        </div>
+                    </div>
+                    <p className="">Création d{"'"}un réseau mutiliste wordpress pour les associations de la Fédération Nationale des Professions Sport et Loisirs. Le réseau doit comprendre 52 sites internet. Permettre de reprendre les anciens URLs</p>
+                </div>
+            </div>
+            <div className="bg-base-200 p-4">
+                <div className="inline-flex items-center justify-between w-full">
+                    <h2 className="font-bold text-2xl my-2">Tâches à réaliser</h2>
+                    <button className="btn btn-primary">Ajouter une tâche</button>
+                </div>
+                <div>
+                    <ul>
+                        <li className="flex gap-3 items-center text-xl my-2"><input type="checkbox"  className="checkbox checkbox-accent" /> Tâche 1</li>
+                        <li className="flex gap-3 items-center text-xl my-2"><input type="checkbox"  className="checkbox checkbox-accent" /> Tâche 2</li>
+                        <li className="flex gap-3 items-center text-xl my-2"><input type="checkbox"  className="checkbox checkbox-accent" /> Tâche 3</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="bg-base-200 p-4">
+                <div className="inline-flex items-center justify-between w-full">
+                    <h2 className="font-bold text-2xl my-2">Notes associées au projet</h2>
+                    <button className="btn btn-primary">Ajouter une note</button>
+                </div>
+                <div>
+                    <div className="card w-80 bg-base-100 shadow-xl my-4">
+                        <div className="card-body">
+                            <h3 className="card-title my-1">Compte rendu de la visio équipe 17/08</h3>
+                            <p className="badge badge-primary font-bold my-1">Suivi de projet</p>
+                            <p>17/08/2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
